c1235704ad6a97738aba63a2e4bf199e9452cdf1
Вељко Лазић <63718639+veljkolazic17@users.noreply.github.com>
2021-11-26 11:50:08 +0100

    Add files via upload

--- genon.c
+++ genon.c
@@ -12,37 +12,43 @@
 #define true 1
 #define false 0
 
-uint8_t get_batch_bin(int m,int batch_size,uint8_t* image,int x,int y){
+uint8_t get_batch_bin(int m,int batch_size,uint8_t* image,int x,int y,int sensitivity){
     int s = (x*batch_size+pow(batch_size,2)*m*y)*NUM_CHANNELS;
     int u = 0;
     int R_sum = 0;
     int G_sum = 0;
     int B_sum = 0;
-
     for(char i = 0;i<batch_size;i++){
         u = batch_size*m*i*NUM_CHANNELS;
         for(char j = 0;j<batch_size;j++){
-            R_sum+=image[u+s+j*NUM_CHANNELS];
-            G_sum+=image[u+s+j*NUM_CHANNELS+1];
-            B_sum+=image[u+s+j*NUM_CHANNELS+2];
+            int pos = j*NUM_CHANNELS+u+s;
+            R_sum+=image[pos];
+            G_sum+=image[pos+1];
+            B_sum+=image[pos+2];
         }
     }
-    R_sum/=pow(batch_size,2);
-    G_sum/=pow(batch_size,2);
-    B_sum/=pow(batch_size,2);
+    int del = pow(batch_size,2);
+    R_sum/=del;
+    G_sum/=del;
+    B_sum/=del;
     int avg = (R_sum+G_sum+B_sum)/3;
-    if(avg > 150) return true;
+    if(avg > 200) return true;
     return false;
 }
 
-// 1->path_in 2->width 3->hegiht 4->path_out
+// 1->path_in 2->nonogram_size 3->path_out 4->sensitivity
 int main(int argc,char**argv){
     
     int width,height,bpp;
-    char* image_path = argv[1];
+    char* path_in = argv[1];
+    char* path_out = argv[3];
+    /*Sensitivity used in interpolation
+    *TODO should implement auto-sensitivity feature
+    */
+    int sensitivity = atoi(argv[4]);
 
     //Number of rows and columns used for interpolation
-    int row_col_cnt = 73;
+    int row_col_cnt = atoi(argv[2]);
 
     //code will only work if pic_size%batch_size==0
     int nonogram_width = row_col_cnt*SIZE_DIFF;
@@ -53,7 +59,7 @@ int main(int argc,char**argv){
     int max = nonogram_height/row_col_cnt;
 
 
-    uint8_t* image = stbi_load("nature.jpg",&width,&height,&bpp,3);
+    uint8_t* image = stbi_load(path_in,&width,&height,&bpp,3);
     uint8_t* nonogram_ready = (uint8_t*)malloc(nonogram_width*nonogram_height*NUM_CHANNELS);
     //Resize
     if(stbir_resize_uint8(image,width,height,0,nonogram_ready,nonogram_width,nonogram_height,0,NUM_CHANNELS)==-1){
@@ -69,21 +75,22 @@ int main(int argc,char**argv){
     
     for(int i = 0;i<row_col_cnt;i++){
         for(int j = 0;j<row_col_cnt;j++){
-            char should_colour = get_batch_bin(row_col_cnt,max,nonogram_ready,i,j);
+            char should_colour = get_batch_bin(row_col_cnt,max,nonogram_ready,i,j,sensitivity);
+            int pos = (j*row_col_cnt+i)*NUM_CHANNELS;
             if(should_colour){
-                result[(j*row_col_cnt+i)*NUM_CHANNELS] = 255;
-                result[(j*row_col_cnt+i)*NUM_CHANNELS+1] = 255;
-                result[(j*row_col_cnt+i)*NUM_CHANNELS+2] = 255;
+                result[pos] = 255;
+                result[pos+1] = 255;
+                result[pos+2] = 255;
             }
             else{
-                result[(j*row_col_cnt+i)*NUM_CHANNELS] = 0;
-                result[(j*row_col_cnt+i)*NUM_CHANNELS+1] = 0;
-                result[(j*row_col_cnt+i)*NUM_CHANNELS+2] = 0;
+                 result[pos] = 0;
+                result[pos+1] = 0;
+                result[pos+2] = 0;
             }
         }
     }
     //Save image
-    if(stbi_write_png("done.png",row_col_cnt,row_col_cnt,NUM_CHANNELS,result,row_col_cnt*NUM_CHANNELS)==-1){
+    if(stbi_write_png(path_out,row_col_cnt,row_col_cnt,NUM_CHANNELS,result,row_col_cnt*NUM_CHANNELS)==-1){
         return -1;
     }
     return 0;
